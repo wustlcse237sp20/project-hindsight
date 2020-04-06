4f6d277f83b8f74a3b61670acde9f68db4366eb3
SomiOgbozor <somiogbozor@wustl.edu>
2020-04-05 23:30:24 -0500

    refactored test file

---
+++ Hindsight/test/DatapointCollectorTest.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatapointCollectorTest {
+    DatapointCollector testee = new DatapointCollector();
+    @org.junit.jupiter.api.Test
+    void testAddPoint() {
+        String[] testPoint = {"TEST", "1234"};
+
+        testee.addPoint(testPoint);
+
+        List<String[]> listPoints = testee.getListPoints();
+
+        assertEquals(1, listPoints.size());
+
+        assertEquals(testPoint[0], listPoints.get(0)[0]);
+        assertEquals(testPoint[1], listPoints.get(0)[1]);
+    }
+}
